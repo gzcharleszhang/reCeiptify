822be14d560cfabc7023c3aa8dc010555d361790
Jackamac-attack <zhangjack27@gmail.com>
2018-09-15 00:59:52 -0400

    added ng bootstrap and routing

--- angular-src/lejr-frontend/src/app/app.module.ts
+++ angular-src/lejr-frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
@@ -8,16 +10,22 @@ import {FirebaseService} from "./services/firebase.service";
 import {MatButtonModule} from "@angular/material";
 import { HomePageComponent } from './home-page/home-page.component';
 
+const routes : Routes = [
+  {path: "**", component: HomePageComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     BrowserAnimationsModule,
     FirebaseModule,
-    MatButtonModule
+    MatButtonModule,
+    NgbModule
   ],
   providers: [FirebaseProvider, FirebaseService],
   bootstrap: [AppComponent]
