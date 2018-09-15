473ca0bc596494112cbee340e757a90fd75f852e
Jackamac-attack <zhangjack27@gmail.com>
2018-09-15 14:04:18 -0400

    splitting will bring you to the friends page

--- angular-src/lejr-frontend/src/app/services/expenses.service.ts
+++ angular-src/lejr-frontend/src/app/services/expenses.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from "@angular/common/http";
 })
 export class ExpensesService {
 
-  private readonly CREATE_EXPENSE_ENDPOINT = "/api/expenses";
+  private readonly CREATE_EXPENSE_ENDPOINT = "http://localhost:8080/api/expenses";
 
   private readonly BUYER_USER_ID = "7291e7e8-1da3-4c23-8594-795f67fa5a65_9de7e105-36ff-4151-9b52-35607f4c50bb";
   private readonly PAYER_USER_ID = "7291e7e8-1da3-4c23-8594-795f67fa5a65_39e0dc13-14d5-4b5c-af5a-49b1dcef34ed";
