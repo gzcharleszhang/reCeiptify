73c2d5f461b8c8dd42c35b63066ac4cdce75c287
Jackamac-attack <zhangjack27@gmail.com>
2018-09-15 09:25:20 -0400

    generate receipt component

---
+++ angular-src/lejr-frontend/src/app/upload-receipt/upload-receipt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-receipt',
+  templateUrl: './upload-receipt.component.html',
+  styleUrls: ['./upload-receipt.component.css']
+})
+export class UploadReceiptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
