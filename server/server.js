489a1d6a9794f73c8ef1a6f1e7ee6c7d7e2371a5
Charles Zhang <gzcharleszhang@hotmail.com>
2018-09-15 21:32:14 -0400

    fixed routing issue

--- server/server.js
+++ server/server.js
@@ -33,8 +33,8 @@ app.set('view engine', 'html');
 if (process.env.NODE_ENV === 'production') {
   app.set('views', path.join(__dirname, '../public'));
   app.use(express.static(path.join(__dirname, '../public')));
-  app.get('/', (req, res) => {
-    res.render('index');
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 }
 
